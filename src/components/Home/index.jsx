e4b8753f30d78a4ce5d076be5e93d25bb14d6d50
Arniwatt Chonkiattipoom <arniwatt.c@ku.th>
2019-06-02 02:50:17 +0700

    add carousel

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -4,11 +4,12 @@ import main_pic from "./main_pic.png";
 import balen from "./balen.jpg";
 import celine from "./celine.jpg";
 import chanel from "./chanel.jpg";
-import coah from "./coah.jpg";
+import fendi from "./fendi.jpg";
 import gucci from "./gucci.jpg";
 import hermes from "./hermes.jpg";
 import louis from "./louis vitt.jpg";
 import prada from "./prada.jpg";
+import Carousel from "react-bootstrap/Carousel";
 
 const Head = {
   position: "absolute",
@@ -43,6 +44,11 @@ const groupImg = {
   margin: "0px 50px 10px 72px"
 };
 
+const brandSize = {
+  width: "1500px"
+};
+const myCarousel = {};
+
 class Home extends Component {
   render() {
     return (
@@ -50,7 +56,51 @@ class Home extends Component {
         <div style={Head} />
         <Image src={main_pic} style={Img} />
         <div style={center}>LABXURY</div>
-        <div>
+        <div style={myCarousel}>
+          <Carousel>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src="holder.js/800x400?text=First slide&bg=373940"
+                alt="First slide"
+              />
+              <Carousel.Caption>
+                <h3>First slide label</h3>
+                <p>
+                  Nulla vitae elit libero, a pharetra augue mollis interdum.
+                </p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src="holder.js/800x400?text=Second slide&bg=282c34"
+                alt="Third slide"
+              />
+
+              <Carousel.Caption>
+                <h3>Second slide label</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src="holder.js/800x400?text=Third slide&bg=20232a"
+                alt="Third slide"
+              />
+
+              <Carousel.Caption>
+                <h3>Third slide label</h3>
+                <p>
+                  Praesent commodo cursus magna, vel scelerisque nisl
+                  consectetur.
+                </p>
+              </Carousel.Caption>
+            </Carousel.Item>
+          </Carousel>
+        </div>
+        <div style={brandSize}>
           <Image.Group>
             <Image src={gucci} style={groupImg} />
             <Image src={prada} style={groupImg} />
@@ -62,7 +112,7 @@ class Home extends Component {
             <Image src={hermes} style={groupImg} />
             <Image src={louis} style={groupImg} />
             <Image src={celine} style={groupImg} />
-            <Image src={coah} style={groupImg} />
+            <Image src={fendi} style={groupImg} />
           </Image.Group>
         </div>
       </Container>

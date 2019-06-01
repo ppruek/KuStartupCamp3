1d48292707c8def717474b856bef21fe89a8c215
Arniwatt Chonkiattipoom <arniwatt.c@ku.th>
2019-06-02 02:18:00 +0700

    add brandname

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,6 +1,14 @@
 import React, { Component } from "react";
-import { Container, Image } from "semantic-ui-react";
+import { Container, Image, Divider } from "semantic-ui-react";
 import main_pic from "./main_pic.png";
+import balen from "./balen.jpg";
+import celine from "./celine.jpg";
+import chanel from "./chanel.jpg";
+import coah from "./coah.jpg";
+import gucci from "./gucci.jpg";
+import hermes from "./hermes.jpg";
+import louis from "./louis vitt.jpg";
+import prada from "./prada.jpg";
 
 const Head = {
   position: "absolute",
@@ -8,7 +16,6 @@ const Head = {
   height: 40,
   left: 0,
   top: 0,
-  //   marginLeft: "40px",
   background: "rgba(12, 16, 20, 0.94)"
 };
 
@@ -17,7 +24,8 @@ const Img = {
   width: "1440px",
   height: "700px",
   marginLeft: "auto",
-  marginRight: "auto"
+  marginRight: "auto",
+  marginBottom: "-5%"
 };
 
 const center = {
@@ -29,6 +37,12 @@ const center = {
   transform: "translate(-50%, -50%)"
 };
 
+const groupImg = {
+  width: "250px",
+  height: "250px",
+  margin: "0px 50px 10px 72px"
+};
+
 class Home extends Component {
   render() {
     return (
@@ -36,6 +50,21 @@ class Home extends Component {
         <div style={Head} />
         <Image src={main_pic} style={Img} />
         <div style={center}>LABXURY</div>
+        <div>
+          <Image.Group>
+            <Image src={gucci} style={groupImg} />
+            <Image src={prada} style={groupImg} />
+            <Image src={chanel} style={groupImg} />
+            <Image src={balen} style={groupImg} />
+          </Image.Group>
+          <Divider hidden />
+          <Image.Group>
+            <Image src={hermes} style={groupImg} />
+            <Image src={louis} style={groupImg} />
+            <Image src={celine} style={groupImg} />
+            <Image src={coah} style={groupImg} />
+          </Image.Group>
+        </div>
       </Container>
     );
   }

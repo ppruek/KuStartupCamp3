94136ed24c7288e46e52944435e01d1b2f280136
Arniwatt Chonkiattipoom <arniwatt.c@ku.th>
2019-06-01 15:55:38 +0700

    our bag

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import React from 'react';
+import React , {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1 id="headname">Deploy Site to be show</h1>
-        <br></br>
-        <h1>We are from group F</h1>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 id="headname">Brandname Bag from everyone</h1>
+          <br></br>
+          <h1>We are from group F</h1>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;

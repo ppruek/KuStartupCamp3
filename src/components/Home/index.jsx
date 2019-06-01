349b0fc289bd4803be47c60d8cb9f3f4efeaa318
Arniwatt Chonkiattipoom <arniwatt.c@ku.th>
2019-06-01 17:00:51 +0700

    template semantic

---
+++ src/components/Home/index.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { Container, Header } from 'semantic-ui-react'
+
+class Home extends Component {
+    render() {
+        return (
+            <Container text>
+                <Header as='h2'>Brandname Bags from everyone</Header>
+                <p>
+                    We are from group F
+                </p>
+                <p>
+                    Underconstructionm
+                </p>
+            </Container>
+        );
+    }
+}
+
+export default Home;
